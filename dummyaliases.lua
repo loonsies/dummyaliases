2d7311bece6593a4b5992c81009a6aaaadd41eb4
senolem <37041587+senolem@users.noreply.github.com>
2026-02-01 13:16:10 +0100

    new commands

--- dummyaliases.lua
+++ dummyaliases.lua
@@ -7,7 +7,11 @@ local chat = require('chat')
 
 local aliases = T {
     '/organize',
-    '/togglefps'
+    '/togglefps',
+    '/pack',
+    '/preppack',
+    '/sneakinvis',
+    '/refreshcondition'
 }
 local state = {
     fastFps = false
@@ -37,10 +41,8 @@ ashita.events.register('command', 'command_cb', function(cmd, nType)
 
                 ashita.tasks.once(5, function()
                     AshitaCore:GetChatManager():QueueCommand(1, '/packer load default')
-                end
-                )
-            end
-            )
+                end)
+            end)
         elseif command == '/togglefps' then
             if state.fastFps == false then
                 state.fastFps = true
@@ -50,6 +52,73 @@ ashita.events.register('command', 'command_cb', function(cmd, nType)
                 state.fastFps = false
                 print(chat.header(addon.name):append(chat.color2(102, 'Fast FPS mode disabled.')))
                 AshitaCore:GetChatManager():QueueCommand(1, '/fps 1')
+            end
+        elseif command == '/preppack' then
+            print(chat.header(addon.name):append(chat.color2(200, 'Preppacking inventory...')))
+            AshitaCore:GetChatManager():QueueCommand(1, '/lac naked 10')
+            ashita.tasks.once(1, function()
+                AshitaCore:GetChatManager():QueueCommand(1, '/lac disable')
+                ashita.tasks.once(1, function()
+                    AshitaCore:GetChatManager():QueueCommand(1, '/lac preppack')
+                end)
+            end)
+        elseif command == '/pack' then
+            print(chat.header(addon.name):append(chat.color2(200, 'Packing inventory...')))
+            AshitaCore:GetChatManager():QueueCommand(1, '/lac naked 10')
+            ashita.tasks.once(1, function()
+                AshitaCore:GetChatManager():QueueCommand(1, '/lac pack')
+            end)
+        elseif command == '/sneakinvis' then
+            local buffs = AshitaCore:GetMemoryManager():GetPlayer():GetBuffs()
+            local sneak = false
+            local invisible = false
+
+            for _, buff in pairs(buffs) do
+                local buffString = AshitaCore:GetResourceManager():GetString('buffs.names', buff)
+                if buffString == 'Sneak' then
+                    sneak = true
+                elseif buffString == 'Invisible' then
+                    invisible = true
+                end
+            end
+
+            if not sneak then
+                print(chat.header(addon.name):append(chat.color2(200, 'Applying Sneak...')))
+                AshitaCore:GetChatManager():QueueCommand(1, '/item "Silent Oil" <me>')
+                if not invisible then
+                    print(chat.header(addon.name):append(chat.color2(200, 'Applying Invisible in 3 seconds...')))
+                    ashita.tasks.once(3, function()
+                        print(chat.header(addon.name):append(chat.color2(200, 'Applying Invisible...')))
+                        AshitaCore:GetChatManager():QueueCommand(1, '/item "Prism Powder" <me>')
+                    end)
+                else
+                    print(chat.header(addon.name):append(chat.color2(200, 'Applying Invisible in 3 seconds...')))
+                    ashita.tasks.once(3, function()
+                        print(chat.header(addon.name):append(chat.color2(200, 'Applying Invisible...')))
+                        AshitaCore:GetChatManager():QueueCommand(1, '/item "Prism Powder" <me>')
+                    end)
+                end
+            else
+                print(chat.header(addon.name):append(chat.color2(200, 'Sneak is already active.')))
+                if not invisible then
+                    print(chat.header(addon.name):append(chat.color2(200, 'Applying Invisible...')))
+                    AshitaCore:GetChatManager():QueueCommand(1, '/item "Prism Powder" <me>')
+                else
+                    print(chat.header(addon.name):append(chat.color2(200, 'Invisible is already active.')))
+                end
+            end
+        elseif command == '/refreshcondition' then
+            local buffs = AshitaCore:GetMemoryManager():GetPlayer():GetBuffs()
+            local refresh = false
+
+            for _, buff in pairs(buffs) do
+                if buff == 43 then
+                    refresh = true
+                end
+            end
+
+            if not refresh then
+                AshitaCore:GetChatManager():QueueCommand(1, '/ma "Refresh" <me>')
             end
         end
     end
